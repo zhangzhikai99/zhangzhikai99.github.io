639e2ff01899999541111b9ef5a02e7cceb7c82c
zhangzhikai99 <73891302+zhangzhikai99@users.noreply.github.com>
2024-01-28 20:02:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
 
 
 

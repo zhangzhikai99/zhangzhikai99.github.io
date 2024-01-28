a3800bda401e2c51dcf6d1b1cd14983038e00271
zhangzhikai99 <73891302+zhangzhikai99@users.noreply.github.com>
2024-01-28 20:04:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 
 
